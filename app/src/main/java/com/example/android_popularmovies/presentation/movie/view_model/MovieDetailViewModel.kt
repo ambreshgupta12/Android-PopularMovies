b14d9983ba265b373efaeeb810b5c4725a62ddee
ashrawat2 <ashish.rawat@publicissapient.com>
2022-06-05 13:32:32 +0530

    Updated MovieDetailViewModel

--- app/src/main/java/com/example/android_popularmovies/presentation/movie/view_model/MovieDetailViewModel.kt
+++ app/src/main/java/com/example/android_popularmovies/presentation/movie/view_model/MovieDetailViewModel.kt
@@ -3,12 +3,11 @@ package com.example.android_popularmovies.presentation.movie.view_model
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.android_popularmovies.data.source.remote.model.MovieDetailsModel
 import com.example.android_popularmovies.domain.usecase.GetMovieDetailsUseCase
 import com.example.android_popularmovies.presentation.movie.state.ResultState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -28,17 +27,18 @@ class MovieDetailViewModel @Inject constructor(
     var job: Job? = null
     fun getMovieDetails(movieId: Int) {
         movieDetailsState.value = ResultState.Loading()
-        job = CoroutineScope(Dispatchers.IO).launch {
+        viewModelScope.launch { }
+        job = viewModelScope.launch {
             val response = getMoviesUseCase.execute(movieId)
             if (response.isSuccessful) {
-                CoroutineScope(Dispatchers.Main).launch {
+                launch {
                     movieDetailsState.value = response.body()
                         ?.let { ResultState.Success(it) }
                 }
             }
         }
         job?.invokeOnCompletion {
-            CoroutineScope(Dispatchers.Main).launch {
+            viewModelScope.launch {
                 movieDetailsState.value =
                     it?.toString()?.let { it1 -> ResultState.Error(it1) }
             }
